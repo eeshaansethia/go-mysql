7df1e17562aca917c4ca6bafddd4af3bf9feed90
Eeshaan Sethia <eeshaans05@gmail.com>
2025-05-19 16:59:57 +0530

    Model complete

--- pkg/models/book.go
+++ pkg/models/book.go
@@ -1,1 +1,25 @@
 package models
+
+import (
+	"go-mysql/pkg/config"
+
+	"github.com/jinzhu/gorm"
+)
+
+var db *gorm.DB
+
+type Book struct {
+	gorm.Model
+	ID     		uint   `json:"id"`
+	Title  		string `json:"title"`
+	Author 		string `json:"author"`
+	Year   		string `json:"year"`
+	ISBN		string `json:"isbn"`
+	Publication string `json:"publication"`
+}
+
+func init(){
+	config.Connect()
+	db = config.GetDB()
+	db.AutoMigrate(&Book{})
+}
